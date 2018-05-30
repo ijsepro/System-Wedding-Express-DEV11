f86a169ccec00fba21e1688cbfe0790ba90b06c3
unknown <hansigunawardhana100@gmail.com>
2018-05-30 22:48:46 +0530

    User Dashbord-Appointmnet

--- WeddingExpress/FrontEnd/src/app/app.module.ts
+++ WeddingExpress/FrontEnd/src/app/app.module.ts
@@ -15,30 +15,30 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatDialogModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule , MatNativeDateModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule,MatTableModule, MatDialogModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule , MatNativeDateModule,MatToolbarModule,MatIconModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UserComponent } from './candidate/user/user.component';
-// import { HomeComponent } from './home/home.component';
-// import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { SignupServiceService } from './services/signup-service.service';
 import { VendorServiceService } from './services/vendor.service';
 import { DashboardComponent } from './candidate/dashboard/dashboard.component';
 import { UserDashboardComponent } from './candidate/user-dashboard/user-dashboard.component';
-import { BudgetCalculatorComponent } from './candidate/budget-calculator/budget-calculator.component';
+import { BudgetCalculatorComponent } from './candidate/appoinment/budget-calculator/budget-calculator.component';
+// import { BudgetCalculatorComponent } from './candidate/budget-calculator/budget-calculator.component';
+// import {BudgetCalculatorComponent} from '.\candidate\appoinment\budget-calculator.component';
 import { VendorDashboardComponent } from './business/vendor-dashboard/vendor-dashboard.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppoinmentComponent } from './candidate/appoinment/appoinment.component';
 import { VendorLoginComponent } from './business/vendor-login/vendor-login.component';
 
-import {MatTableModule} from '@angular/material/table';
+// import {MatTableModule} from '@angular/material/table';
 import { AccSettingsComponent } from './candidate/acc-settings/acc-settings.component';
 import { DeleteAccComponent } from './candidate/delete-acc/delete-acc.component';
 
-import { AgmCoreModule } from '@agm/core';
+// import { AgmCoreModule } from '@agm/core';
 import { VendorSignupComponent } from './business/vendor-signup/vendor-signup.component';
 
 @NgModule({
@@ -51,7 +51,7 @@ import { VendorSignupComponent } from './business/vendor-signup/vendor-signup.co
     SignupComponent,
     DashboardComponent,
     UserDashboardComponent,
-    BudgetCalculatorComponent,
+    BudgetCalculatorComponent ,
     VendorDashboardComponent,
     FooterComponent,
     AppoinmentComponent,
@@ -72,9 +72,9 @@ import { VendorSignupComponent } from './business/vendor-signup/vendor-signup.co
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDz3cM9XMBuNHJW6IJYFRr2HY_c8OmSi2Y&callback=initMap'
-    }),
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyDz3cM9XMBuNHJW6IJYFRr2HY_c8OmSi2Y&callback=initMap'
+    // }),
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'user', component: UserComponent},
@@ -96,6 +96,9 @@ import { VendorSignupComponent } from './business/vendor-signup/vendor-signup.co
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+
     MatCheckboxModule, MatButtonModule, MatDialogModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule,
     MatInputModule 
   ],
